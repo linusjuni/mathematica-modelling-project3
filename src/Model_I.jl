96097bf6b7a3fb3c0a866096069319133e38bc42
Simon Bom <s.bom2142@gmail.com>
2025-04-05 11:06:31 +0200

    new update

---
+++ src/Model_I.jl
@@ -0,0 +1,32 @@
+# Model I
+function solveIP(H, K)
+    h = length(H)
+    myModel = Model(Cbc.Optimizer)
+    # If your want ot use GLPK instead use:
+    #myModel = Model(GLPK.Optimizer)
+
+    A = constructA(H,K)
+
+    @variable(myModel, x[1:h], Bin )
+    @variable(myModel, R[1:h] >= 0 )
+
+    @objective(myModel, Min, sum(x[j] for j=1:h) )
+
+    @constraint(myModel, [j=1:h],R[j] >= H[j] + 10 )
+    @constraint(myModel, [i=1:h],R[i] == sum(A[i,j]*x[j] for j=1:h) )
+
+    optimize!(myModel)
+
+    if termination_status(myModel) == MOI.OPTIMAL
+        println("Objective value: ", JuMP.objective_value(myModel))
+        println("x = ", JuMP.value.(x))
+        println("R = ", JuMP.value.(R))
+    else
+        println("Optimize was not succesful. Return code: ", termination_status(myModel))
+    end
+    x = JuMP.value.(x)
+    R = JuMP.value.(R)
+    return x, R
+end
+
+x, R = solveIP(H,K)
---
+++ src/Model_II.jl
@@ -0,0 +1,39 @@
+# Solve second one
+function solveIP2(H, K)
+    h = length(H)
+    # myModel = Model(Cbc.Optimizer)
+    # If your want ot use GLPK instead use:
+    myModel = Model(GLPK.Optimizer)
+
+    A = constructA(H,K)
+
+    @variable(myModel, x[1:h], Bin )
+    @variable(myModel, U[1:h], Bin )
+    @variable(myModel, R[1:h] >= 0 )
+
+    @objective(myModel, Min, sum(abs(U)) for j=1:h )
+
+
+    @constraint(myModel, [j=1:h], U < R[j] - H[j] - 10 < )
+    @constraint(myModel, [j=1:h], sum(R[j] - H[j] - 10) <= 0)
+
+
+    @constraint(myModel, [j=1:h],R[j] >= H[j] + 10 )
+    @constraint(myModel, [i=1:h],R[i] == sum(A[i,j]*x[j] for j=1:h) )
+
+    optimize!(myModel)
+
+    if termination_status(myModel) == MOI.OPTIMAL
+        println("Objective value: ", JuMP.objective_value(myModel))
+        println("x = ", JuMP.value.(x))
+        println("R = ", JuMP.value.(R))
+    else
+        println("Optimize was not succesful. Return code: ", termination_status(myModel))
+    end
+    x = JuMP.value.(x)
+    R = JuMP.value.(R)
+    return x, R
+end
+
+
+x, R = solveIP2(H,K)
